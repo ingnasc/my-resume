e2b0e256dc1769253b3f9c671e9aa20f54e59a5d
Ingrid Nascimento <ingcnasc@gmail.com>
2025-07-14 13:27:32 -0300

    tentativa de correção para o gerador de pdf

--- script.js
+++ script.js
@@ -18,37 +18,3 @@ toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   toggleBtn.textContent = body.classList.contains('dark-mode') ? '☀️ Modo claro' : '🌙 Modo escuro';
 });
-
-window.addEventListener('DOMContentLoaded', () => {
-  document.getElementById('savePdfBtn').addEventListener('click', () => {
-    const content = document.getElementById('content');
-    document.body.classList.add('printing');
-
-    html2canvas(content, { scale: 2 }).then(canvas => {
-      const imgData = canvas.toDataURL('image/png');
-      const { jsPDF } = window.jspdf;
-      const pdf = new jsPDF('p', 'mm', 'a4');
-      const pageWidth = 210;
-      const pageHeight = 297;
-      const imgWidth = pageWidth;
-      const imgHeight = (canvas.height * imgWidth) / canvas.width;
-
-      let heightLeft = imgHeight;
-      let position = 0;
-
-      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-      heightLeft -= pageHeight;
-
-      while (heightLeft > 0) {
-        position -= pageHeight;
-        pdf.addPage();
-        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
-        heightLeft -= pageHeight;
-      }
-
-      pdf.save('curriculo--ingrid-nascimento.pdf');
-
-      document.body.classList.remove('printing');
-    });
-  });
-});
