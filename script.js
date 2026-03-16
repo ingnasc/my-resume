15d46f03320bde59993c4b7837240ca5bd51730d
Ingrid Nascimento <ingcnasc@gmail.com>
2026-03-16 12:25:35 -0300

    update courses list

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//funcao idade
 function currentAge() {
   const birthDate = new Date('1995-12-23');
   const currentDate = new Date();
@@ -11,10 +12,51 @@ function currentAge() {
 
 document.getElementById('info').innerHTML = `BRASILEIRA, SOLTEIRA, ${currentAge()} ANOS`;
 
+//funcao troca de modos da página
 const toggleBtn = document.getElementById('toggle-dark-mode');
 const body = document.body;
 
 toggleBtn.addEventListener('click', () => {
   body.classList.toggle('dark-mode');
   toggleBtn.textContent = body.classList.contains('dark-mode') ? '☀️ Modo claro' : '🌙 Modo escuro';
+});
+
+//funcao botao cursos
+const botoes = document.querySelectorAll(".toggle-cursos");
+botoes.forEach(botao => {
+
+  botao.addEventListener("click", () => {
+
+    const lista = botao.nextElementSibling;
+
+    if (lista.style.display === "block") {
+      lista.style.display = "none";
+      botao.textContent = "📚 VER CURSOS ▸";
+    } else {
+      lista.style.display = "block";
+      botao.textContent = "📚 OCULTAR CURSOS ▾";
+    }
+
+  });
+
+});
+
+//funcao botao descrição
+const botoesDescricao = document.querySelectorAll(".toggle-curso");
+botoesDescricao.forEach(botao => {
+
+  botao.addEventListener("click", () => {
+
+    const lista = botao.nextElementSibling;
+
+    if (lista.style.display === "block") {
+      lista.style.display = "none";
+      botao.textContent = "📚 VER DESCRIÇÃO ▸";
+    } else {
+      lista.style.display = "block";
+      botao.textContent = "📚 OCULTAR DESCRIÇÃO ▾";
+    }
+
+  });
+
 });
